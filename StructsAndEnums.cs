dc003a178ade0a469a84897ace2adb7d6e7c7cda
Tri Nguyen <tringuyen.nnt@gmail.com>
2016-03-20 22:18:13 +0700

    update Enum and APIs

--- StructsAndEnums.cs
+++ StructsAndEnums.cs
@@ -1,6 +1,29 @@
-﻿using System;
+using System;
+using ObjCRuntime;
 
 namespace SVProgressHUDBinding
 {
-}
+	[Native]
+	public enum SVProgressHUDStyle : long
+	{
+		Light,
+		Dark,
+		Custom
+	}
 
+	[Native]
+	public enum SVProgressHUDMaskType : ulong
+	{
+		None = 1,
+		Clear,
+		Black,
+		Gradient
+	}
+
+	[Native]
+	public enum SVProgressHUDAnimationType : ulong
+	{
+		Flat,
+		Native
+	}
+}
